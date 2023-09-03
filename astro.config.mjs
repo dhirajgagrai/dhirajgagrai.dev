d7ea6f2ddaf16983074c3f27a6e7b640da0fbc0b
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2023-09-03 11:39:14 +0530

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dhirajgagrai.dev',
     integrations: [sitemap()],
-    experimental: {
-        assets: true
-    }
+    output: 'static',
+    adapter: vercel({
+        analytics: true,
+      }),
 });
