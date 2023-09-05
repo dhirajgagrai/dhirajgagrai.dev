abb0a60cd8937558bf0a90b2706e05b3e06d73e5
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2023-09-05 16:50:19 +0530

    Revert added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dhirajgagrai.dev',
     integrations: [sitemap()],
     output: 'static',
-    adapter: vercel({
-        analytics: true,
-      }),
 });
